fb8891a3d56849835116c66cdfa7fdcfefe47f6f
Marco <Filanninobusiness@gmail.com>
2024-12-02 15:18:19 +0100

    Aggiunto commento

--- Componenti/vite-project/src/index.jsx
+++ Componenti/vite-project/src/index.jsx
@@ -2,3 +2,5 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 
 createRoot(document.getElementById("root")).render(<App />);
+
+//Si possono aggiungere sia il componente hello, piu volte all'interno di App, sia Message in app, react eseguirà tutto se gli elementi vengono importati correttamente, seguendo il flusso.
