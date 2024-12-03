f3bba4dbd1de68ec212dc869bb77610eac7ac83c
Marco <Filanninobusiness@gmail.com>
2024-12-03 10:42:10 +0100

    modifiche

--- Componenti/vite-project/src/Messaggio.jsx
+++ Componenti/vite-project/src/Messaggio.jsx
@@ -1,4 +1,22 @@
 function Messaggio() {
-  return <p>Che bella giornata!</p>;
+  const messaggi = [
+    "Oggi fa caldo",
+    " Fa molto freddo",
+    "Fuori piove",
+    "Sta nevicando",
+    "Il cielo è nuvoloso",
+  ];
+
+  const indice = Math.floor(Math.random() * messaggi.length);
+
+  return (
+    <>
+      {!messaggi[indice] ? (
+        <p style={{ color: "red" }}>Errore</p>
+      ) : (
+        <p>{messaggi[indice]}</p>
+      )}
+    </>
+  );
 }
 export default Messaggio;
